362d6a3af41d9ec3853a4b2dc975ee5d71d2e23d
notnoc <89336139+notnoc@users.noreply.github.com>
2022-08-27 14:01:29 +0900

    Create Functions_Landscape.kt

---
+++ Functions_Landscape.kt
@@ -0,0 +1,10 @@
+fun main() {
+  // Write your code below
+var area = fun(base: Int, height: Int): Int{
+  return (base * height) / 2
+}
+var perimeter = {side1: Int, side2: Int -> (side1 + side2) * 2}
+
+  println(area(15,19))
+  println(perimeter(15,24))
+}
