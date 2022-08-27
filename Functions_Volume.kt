ad7eea67e5bfc029e974496d5f55694f2fa2b70e
notnoc <89336139+notnoc@users.noreply.github.com>
2022-08-27 13:54:37 +0900

    Create Functions_Volume.kt

---
+++ Functions_Volume.kt
@@ -0,0 +1,14 @@
+// Write your code below
+fun pyramidVolume(l: Int, w: Int, h: Int) = (l * w * h) / 3
+
+
+fun main() {
+  var length = 5
+  var width = 8
+  var height = 14
+  var volume = pyramidVolume(length, width, height)
+  println("The volume of this pyramid is $volume")
+  // Write more code below
+
+  
+}
