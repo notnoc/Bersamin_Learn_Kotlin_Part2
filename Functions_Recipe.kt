54ff817639a9bcc565ea4cd646544eefcecd47ea
notnoc <89336139+notnoc@users.noreply.github.com>
2022-08-27 13:47:26 +0900

    Create Functions_Recipe.kt

---
+++ Functions_Recipe.kt
@@ -0,0 +1,13 @@
+// Write your code below
+fun ozToTsp(oz: Double): Double{
+  val tsp = oz * 6
+  return tsp
+}
+
+
+fun main() {
+  // Write more code below
+var tspNeeded = ozToTsp(0.75)
+println("You will need $tspNeeded tsp of vanilla extract for this recipe")
+  
+}
